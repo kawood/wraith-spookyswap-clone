3e2ef234aa408ec3252640ff2a9e7cff1a2ae9d2
minhlq <minhmeo753@gmail.com>
2021-12-11 03:21:05 +0700

    feat: make header

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,32 @@
 import { NextPage } from 'next';
 
 const Layout: NextPage = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <div className='bg-slate-800 flex items-center'>
+        <HeadItem icon='/swap.svg' text='Swap' />
+        <HeadItem icon='/liquidity.svg' text='Liquidity' />
+        <HeadItem icon='/farm.svg' text='Farm' />
+        <HeadItem icon='/stake.svg' text='Stake BOO' />
+        <HeadItem icon='/bridge.svg' text='Bridge' />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+type HeadItemProps = {
+  text: string;
+  icon: string;
+};
+
+const HeadItem = ({ text, icon }: HeadItemProps): JSX.Element => {
+  return (
+    <div className='text-white px-3 py-3 hover:bg-slate-700 cursor-default flex'>
+      <img className='mr-2' src={icon} />
+      {text}
+    </div>
+  );
 };
 
 export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import type { NextPage } from 'next';
 import { InputText } from 'primereact/inputtext';
 import { useState } from 'react';
+import Layout from '../components/layout';
 
 const Home: NextPage = () => {
   const [value1, setValue1] = useState('');
   return (
-    <InputText
-      className='m-10'
-      value={value1}
-      onChange={(e) => setValue1(e.target.value)}
-    />
+    <Layout>
+      <InputText
+        className='m-10'
+        value={value1}
+        onChange={(e) => setValue1(e.target.value)}
+      />
+    </Layout>
   );
 };
 
